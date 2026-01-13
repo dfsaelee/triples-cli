d87bc3a41c6c58683520c1d6ab9b297f8f87a9b1
dfsaelee <dsaltac@gmail.com>
2026-01-12 21:49:45 -0800

    refactored code

--- main.go
+++ main.go
@@ -31,30 +31,25 @@ type PlaylistItems struct {
 	} `json:"items"`
 }
 
+// clean the title
 func cleanTitle(title string) string {
 	// Remove hashtags and the text immediately following them
 	re := regexp.MustCompile(`#\S+`)
 	cleaned := re.ReplaceAllString(title, "")
-
 	// Remove extra spaces
 	cleaned = strings.TrimSpace(cleaned)
 	cleaned = regexp.MustCompile(`\s+`).ReplaceAllString(cleaned, " ")
-
 	return cleaned
 }
 
-func main() {
-	// load env, make sure you export api key
-	apiKey := os.Getenv("YOUTUBE_API_KEY")
-	if apiKey == "" {
-		fmt.Fprintln(os.Stderr, "ERROR: YOUTUBE_API_KEY is not set")
-		os.Exit(1)
-	}
-	channelRes, err := http.Get("https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle=triplescosmos&key=" + apiKey + "&maxResults=5")
+// Get the playlist id
+func getPlaylistId(apiKey, channelHandle string) (string, error) {
+	url := fmt.Sprintf("https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle=" + channelHandle + "&key=" + apiKey + "&maxResults=5")
+	channelRes, err := http.Get(url)
 
 	// if invalid request
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 	defer channelRes.Body.Close()
 	if channelRes.StatusCode != 200 {
@@ -64,21 +59,24 @@ func main() {
 	// get response body
 	channelsBody, err := io.ReadAll(channelRes.Body)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
 	// unmarshal channels
 	var channel ChannelItems
-	json.Unmarshal(channelsBody, &channel)
-	uploadsPlaylistId := channel.Items[0].ContentDetails.RelatedPlaylists.Uploads
-
-	// get videos from playlist
-	playlistRes, err := http.Get("https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&key=" + apiKey + "&playlistId=" + uploadsPlaylistId + "&maxResults=1")
-
-	// if invalid request
-	if err != nil {
-		panic(err)
+	if err := json.Unmarshal(channelsBody, &channel); err != nil {
+		return "", err
 	}
+	return channel.Items[0].ContentDetails.RelatedPlaylists.Uploads, nil
+}
+
+func getPlaylistItems(apiKey, playlistId string) (title, videoId string, err error) {
+	url := fmt.Sprintf("https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&key=%s&playlistId=%s&maxResults=1", apiKey, playlistId)
+	playlistRes, err := http.Get(url)
+	if err != nil {
+		return "", "", err
+	}
+
 	defer playlistRes.Body.Close()
 	if playlistRes.StatusCode != 200 {
 		fmt.Println("Youtube PlaylistItems API not Available")
@@ -87,16 +85,39 @@ func main() {
 	// get body
 	playlistItemsBody, err := io.ReadAll(playlistRes.Body)
 	if err != nil {
-		panic(err)
+		return "", "", err
 	}
 
 	// ummarshal
 	var playlistItems PlaylistItems
 
 	json.Unmarshal(playlistItemsBody, &playlistItems)
-	videoTitle := playlistItems.Items[0].Snippet.Title
-	videoId := playlistItems.Items[0].Snippet.ResourceId.VideoId
+	title = playlistItems.Items[0].Snippet.Title
+	videoId = playlistItems.Items[0].Snippet.ResourceId.VideoId
+	title = cleanTitle(title)
+	return title, videoId, nil
 
-	fmt.Println(cleanTitle(videoTitle))
+}
+
+func main() {
+	channelHandle := "triplescosmos"
+	// load env, make sure you export api key
+	apiKey := os.Getenv("YOUTUBE_API_KEY")
+	if apiKey == "" {
+		fmt.Fprintln(os.Stderr, "ERROR: YOUTUBE_API_KEY is not set")
+		os.Exit(1)
+	}
+
+	uploadsPlaylistId, err := getPlaylistId(apiKey, channelHandle)
+	if err != nil {
+		panic(err)
+	}
+
+	videoTitle, videoId, err := getPlaylistItems(apiKey, uploadsPlaylistId)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(videoTitle)
 	fmt.Printf("Enjoy Your Content! https://www.youtube.com/watch?v=%s\n", videoId)
 }
